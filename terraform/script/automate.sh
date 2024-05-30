c22fe7340d79fe9a8696a1fc30dcc2e2d437a6bb
Ujwal budha <ujjwal.budha07@gmail.com>
2024-05-30 22:17:09 +0545

    updated user data

--- terraform/script/automate.sh
+++ terraform/script/automate.sh
@@ -5,4 +5,5 @@ sudo amazon-linux-extras install epel -y
 sudo yum install ansible -y
 git clone https://github.com/UjjwalBudha/ansible.git
 cd ansible
+ansible-playbook --version
 ansible-playbook --connection=local --inventory 127.0.0.1, playbook.yml
